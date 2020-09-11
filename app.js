8dbe26a2a101c7ec1325dd7178ec1753e674bdd6
natumenonomi <nonegemu3e@gmail.com>
2020-09-11 17:38:00 +0900

    Streamの実装まで一時保存

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-'use strict';
+'use strict';//strict(厳格モード）
+
+/*Node.jsからモジュール呼び出し*/
+const fs =require('fs');//fs=FileSystem　の宣言
+const readline =require('readline');//ファイルを1行ずつ読み込むモジュール　の宣言
+
+const rs =fs.createReadStream('./popu-pref.csv');//popu-pref.csvファイルからファイル読み込みを行うStream生成
+const rl = readline.createInterface({ input: rs, output: {}});
+//readlineオブジェクトのinputとして設定しrlオブジェクトを作成
+
+rl.on('line',lineString => {
+  console.log(lineString);
+});
+//rlオブジェクトでlineというイベントが発生したら　無名関数を呼ぶ。lineイベントが発生しら、コンソールに引数line Stringの内容（読み込んだ1行）が出力される。
