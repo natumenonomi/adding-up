57ac2987f05d9f92d55b92817bece31974cc3999
natumenonomi <nonegemu3e@gmail.com>
2020-09-11 18:06:08 +0900

    ファイルからデータを抜き出す

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';//strict(厳格モード）
+/*ファイルを1行ずつ読み込む*/
 
 /*Node.jsからモジュール呼び出し*/
 const fs =require('fs');//fs=FileSystem　の宣言
@@ -9,6 +10,17 @@ const rl = readline.createInterface({ input: rs, output: {}});
 //readlineオブジェクトのinputとして設定しrlオブジェクトを作成
 
 rl.on('line',lineString => {
-  console.log(lineString);
+  const columns =lineString.split(',');
+  //引数lineStringから与えられた文字列を,で分割して、それをcolumnsという名前の配列にしている
+  const year = parseInt(columns[0]);
+  //集計年を変数に保存　文字列を整数値に変換するparseInt使用
+  const prefecture =columns[1];
+  //都道府県名を変数に保存
+  const popu =parseInt(columns[3]);
+  //15～19歳の人口を変数に保存　文字列を整数値にするperseInt使用
+  if (year === 2010 || year ===2015){
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
 });
-//rlオブジェクトでlineというイベントが発生したら　無名関数を呼ぶ。lineイベントが発生しら、コンソールに引数line Stringの内容（読み込んだ1行）が出力される。
